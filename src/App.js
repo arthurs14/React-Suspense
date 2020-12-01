a546f2bf543e004d173beaffc695d36d63d947ee
Arthur Sandro <arthurcs426@gmail.com>
2020-11-30 17:55:09 -0800

    another way to example the lazy load

--- src/App.js
+++ src/App.js
@@ -7,23 +7,35 @@ import Welcome from './containers/Welcome';
 const Posts = React.lazy(() => import('./containers/Posts'));
 
 class App extends Component {
+  state = { show: false };
+
+  modeHandler = () => {
+    this.setState(prevState => {
+      return { show: !prevState.show };
+    });
+  };
+
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Posts />
-            </Suspense>
-          )} />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <button onClick={this.modeHandler}>Toggle Mode</button>
+        {this.state.show ? <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense> : <User />}
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route path="/posts" render={() => (
+      //       <Suspense fallback={<div>Loading...</div>}>
+      //         <Posts />
+      //       </Suspense>
+      //     )} />
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
